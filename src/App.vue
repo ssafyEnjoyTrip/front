ba116ca08d8bf8871362d100141bad9940dcc7d8
taeho <ice9880@gmail.com>
2023-11-09 17:48:20 +0900

    vue init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import NavbarPage from "./components/NavbarPage.vue";
+import FooterPage from "./components/FooterPage.vue";
+</script>
+
+<template>
+    <navbar-page></navbar-page>
+    <footer-page></footer-page>
+</template>
+
+<style scoped></style>
---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+// import { createRouter, createWebHistory } from "vue-router";
+// import CafePage from "@/components/CafePage.vue";
+// import BlogPage from "@/components/BlogPage.vue";
+// import MailPage from "@/components/MailPage.vue";
+// import MainPage from "@/components/MainPage.vue";
+
+// const router = createRouter({
+//     history: createWebHistory(import.meta.env.BASE_URL),
+//     routes: [
+//         {
+//             path: "/",
+//             component: MainPage,
+//         },
+//         {
+//             path: "/cafe",
+//             component: CafePage,
+//         },
+//         {
+//             path: "/blog",
+//             component: BlogPage,
+//         },
+//         {
+//             path: "/mail",
+//             component: MailPage,
+//         },
+//     ],
+// });
+
+// export default router;
